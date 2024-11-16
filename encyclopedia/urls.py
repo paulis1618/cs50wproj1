9d5165604037260e81b5bb1323ba0189ab7136a9
Παύλος Νικολού <nikoloup@Air-Paulos.router.wind>
2024-11-16 02:24:44 +0200

    2.1

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.page, name="page"),
+    path("search/", views.search, name="search"),
+    path("random/", views.random, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,62 @@
 from django.shortcuts import render
 
 from . import util
-
+import markdown2
+import entries
+from random import choice
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def md_to_html(title):
+    page = util.get_entry(title)
+    if page == None:
+        return None
+    else:
+        return markdown2.markdown(page)
+    
+def page(request, title):
+    title2 = md_to_html(title)
+    if title2 == None:
+        return render(request,"encyclopedia/error.html" )
+    else:
+        return render(request, "encyclopedia/page.html", {
+            "name": title2,
+            "tit" : title.capitalize()
+        } )
+        
+def search(request):
+    if request.method == "POST":
+        entry_search = request.POST['q']
+        item = md_to_html(entry_search)
+        if item != None:
+            return render(request, "encyclopedia/page.html", {
+                "tit" : entry_search,
+                "name" : item
+            })
+        else:
+            pages_filt = []
+            entries2 = util.list_entries()
+            entry_search2 = entry_search.upper()
+            for i in entries2:
+                kefalaia = i.upper()
+                if entry_search2 in kefalaia:
+                    pages_filt.append(i)
+            return render(request, "encyclopedia/filtpages.html", {
+                "filtered_pages" : pages_filt, 
+                "filter_phrase" : entry_search
+            })
+                    
+                
+def random(request):
+    entries_stored = util.list_entries()
+    rand_page = choice(entries_stored)
+    
+    return render(request, "encyclopedia/page.html", {
+        "tit" :  rand_page ,
+        "name" : md_to_html(rand_page)
+    })
+                
+            
